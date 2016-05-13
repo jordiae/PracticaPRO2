9c4bb85b8cd111ad14b0b839c94d833895ce3abd
Jordi Armengol Estape <jordi.armengol.estape@a5s104mac53.fib.upc.es>
2016-05-13 11:44:59 +0200

    Cjt_autors::afegir_cita v1

--- Cjt_autors/afegir_cita.cc
+++ Cjt_autors/afegir_cita.cc
@@ -1,1 +1,11 @@
 #include "Cjt_autors.hh"
+
+void afegir_cita() {
+	int frase_inicial;
+  	int frase_final;
+  	vector<string> frases;
+  	cin >> frase_inicial >> frase_final;
+  	Text text = *it1;
+  	frases = text.frases_x_fins_y(frase_inicial, frase_final);
+  	cites.afegir_cita(frase_inicial, frase_final, frases);
+}
