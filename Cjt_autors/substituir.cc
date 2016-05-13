14e976abd55d73ca9f893d77453f10af84814d50
Jordi Armengol Estape <jordi.armengol.estape@a5s104mac53.fib.upc.es>
2016-05-13 13:39:33 +0200

    Cjt_autors::substituir v1

--- Cjt_autors/substituir.cc
+++ Cjt_autors/substituir.cc
@@ -1,1 +1,7 @@
 #include "Cjt_autors.hh"
+
+Cjt_autors::substituir() {
+	string paraula_a_substituir, paraula_que_substitueix;
+	cin >> string paraula_a_substituir >> paraula_que_substitueix;
+	(*it1).substituir(paraula_a_substituir,paraula_que_substitueix);
+}
