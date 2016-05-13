a765ef71aef827fd8b327384433ad1573eda051d
Jordi Armengol Estape <jordi.armengol.estape@a5s104mac53.fib.upc.es>
2016-05-13 13:32:05 +0200

    Cjt_autors::afegir_cita v3: eliminada còpia innecessària de text

--- Cjt_autors/afegir_cita.cc
+++ Cjt_autors/afegir_cita.cc
@@ -5,7 +5,6 @@ void Cjt_autors::afegir_cita() {
   	int frase_final;
   	vector<string> frases;
   	cin >> frase_inicial >> frase_final;
-  	Text text = *it1;
-  	frases = text.frases_x_fins_y(frase_inicial, frase_final);
+  	frases = (*it1).frases_x_fins_y(frase_inicial, frase_final);
   	cites.afegir_cita(frase_inicial, frase_final, frases);
 }
