32abf2d866a9ad60bbedb39b16d6ac8136f5cc24
Jordi Armengol Estape <jordi.armengol.estape@a5s104mac53.fib.upc.es>
2016-05-13 13:27:02 +0200

    Cjt_autors:imprimeix_tots_textos_autor v1

--- Cjt_autors/imprimeix_tots_textos_autor.cc
+++ Cjt_autors/imprimeix_tots_textos_autor.cc
@@ -1,1 +1,9 @@
 #include "Cjt_autors.hh"
+
+Cjt_autors::imprimeix_tots_textos_autor() {
+	string autor;
+	cin >> autor;
+	map<string, Text>::iterator it; 
+	for (it = *it2 -> begin(); it != *it2 -> end(); it++)
+		cout << it -> first << endl;
+}
