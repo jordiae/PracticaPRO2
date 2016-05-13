5784fa2bf60f6c8511b2bff785effdf8672f8ea7
Jordi Armengol Estape <jordi.armengol.estape@a5s104mac53.fib.upc.es>
2016-05-13 12:28:38 +0200

    Cjt_autors::frases v1

--- Cjt_autors/frases.cc
+++ Cjt_autors/frases.cc
@@ -1,1 +1,29 @@
 #include "Cjt_autors.hh"
+
+Cjt_autors::frases() {
+	char c;
+	cin >> c;
+	Text text = *it1;
+	if (c == '(') {
+		string expressio;
+		getline(cin,expressio)
+		expressio += '('; 
+		expressio.pop_back();
+		expressio.pop_back();
+		text.imprimeix_frases_expressio(expressio);
+	}
+	else {
+		list<string> paraules;
+		list<string>::iterator it = paraules.begin();
+		string paraula;
+		cin >> paraula;
+		while (paraula[paraula.size()-1] != '"') {
+			paraules.insert(it,paraula)
+			++it;
+			cin >> paraula;
+		}
+		paraula.pop_back();
+        paraules.insert(iy,paraula);
+		text.imprimeix_frases_paraules(paraules);
+	}
+}
