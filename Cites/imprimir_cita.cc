cbf2d2bd0392f67469de29ad1be403e691362299
Jordi Armengol Estapé <me@jordiae.com>
2016-05-15 17:00:20 +0200

    Cites::imprimir_cita v1

--- Cites/imprimir_cita.cc
+++ Cites/imprimir_cita.cc
@@ -1,1 +1,10 @@
 #include "Cites.hh"
+
+Cites::imprimir_cita(string referencia) {
+	map<string, int>::const_iterator i = cites.find(referencia);
+	cout << (*i).autor << endl << (*i).titol_text << endl << (*i).frase_inicial << endl << (*i).frase_final << endl;
+	int n = frases.size();
+	for (int i = 0; i < n; i++)
+	    cout << frases[i];
+	cout << endl; 
+}
