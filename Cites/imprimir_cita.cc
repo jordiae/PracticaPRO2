7a7418249ee47d0525a03ad32b2ee8ff6930259a
Jordi Armengol Estapé <me@jordiae.com>
2016-05-15 17:07:44 +0200

    Cites::imprimir_cita v2 corregit error de punters

--- Cites/imprimir_cita.cc
+++ Cites/imprimir_cita.cc
@@ -2,9 +2,9 @@
 
 Cites::imprimir_cita(string referencia) {
 	map<string, int>::const_iterator i = cites.find(referencia);
-	cout << (*i).autor << endl << (*i).titol_text << endl << (*i).frase_inicial << endl << (*i).frase_final << endl;
-	int n = frases.size();
+	cout << (*i).second.autor << endl << (*i).second.titol_text << endl << (*i).second.frase_inicial << endl << (*i).second.frase_final << endl;
+	int n = (*i).second.frases.size();
 	for (int i = 0; i < n; i++)
-	    cout << frases[i];
+	    cout << (*i).second.frases[i];
 	cout << endl; 
 }
