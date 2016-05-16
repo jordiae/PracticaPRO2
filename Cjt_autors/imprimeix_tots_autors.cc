5af6bc26499f2e8a0dda3567031174850be04fb0
BuildTools <guillem.ferrer.nicolas@gmail.com>
2016-05-16 20:35:16 +0200

    v1.0 of file

--- Cjt_autors/imprimeix_tots_autors.cc
+++ Cjt_autors/imprimeix_tots_autors.cc
@@ -1,5 +1,7 @@
 #include "Cjt_autors.hh"
 
 void Cjt_autors::imprimeix_tots_autors() {
-
+	map<string, map<string, Text> >::iterator it;
+	for (it = Mautors.begin(); it != Mautors.end(); it++)
+		cout << it -> first << endl;
 }
