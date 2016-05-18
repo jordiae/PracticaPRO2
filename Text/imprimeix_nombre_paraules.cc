1814167314055af961092895fdd174059f3ffa18
Jordi Armengol Estapé <me@jordiae.com>
2016-05-18 13:47:50 +0200

    Text::imprimeix_nombre_paraules v1

--- Text/imprimeix_nombre_paraules.cc
+++ Text/imprimeix_nombre_paraules.cc
@@ -1,5 +1,13 @@
 #include "Text.hh"
 
-void Text::imprimeix_nombre_paraules(){
-
+void Text::imprimeix_nombre_paraules() {
+    int c = 1;
+	int n = linies.size();
+	for (int i = 0; i < n; i++) {
+		int m = linies[i].size();
+		for (int j = 0; j < m; j++)
+			if (linies[i][j] == 32)
+				c++;
+	}
+   cout << c << endl;
 }
