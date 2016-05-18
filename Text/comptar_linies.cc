31f71a09df0b18011146eb784da17145f54c7c86
Jordi Armengol Estapé <me@jordiae.com>
2016-05-18 13:39:25 +0200

    Text::comptar_linies v2: list -> vector

--- Text/comptar_linies.cc
+++ Text/comptar_linies.cc
@@ -1,8 +1,5 @@
 #include "Text.hh"
 
 int Text::comptar_linies() {
-	int n = 0;
-	for (list<string>::const_iterator i = linies.begin(); i != linies.end(); i++)
-		n++;
-	return n;
+	return linies.size();
 }
