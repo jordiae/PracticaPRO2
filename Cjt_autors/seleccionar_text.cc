1c09d87623fe670e50cf581985c88a944a87d778
BuildTools <guillem.ferrer.nicolas@gmail.com>
2016-05-22 00:07:56 +0200

    Nuts and Bolts
    Fixed conte_paraula and got some work done on seleccionar_text

--- Cjt_autors/seleccionar_text.cc
+++ Cjt_autors/seleccionar_text.cc
@@ -1,5 +1,27 @@
 #include "Cjt_autors.hh"
 
 void Cjt_autors::seleccionar_text() {
+    vector <string> paraules;
+    string paraula;
+    while (cin >> paraula && paraula[paraula.size() - 1] != '}'){
+        paraules.push_back(paraula);
+    }
+    paraula.erase(paraula.size() - 1, 1);
+    paraules.push_back(paraula);
 
+    map<string, Text>::iterator iterator1;
+    map<string, map<string, Text> >::iterator iterator2;
+
+    for(iterator2 = Mautors.begin(); iterator2 != Mautors.end(); iterator2){
+        vector <string> temp1 = paraules;
+        vector <string> temp2;
+        for (int i = 0; i < temp1.size(); i++){
+            if (! Text.conte_paraula((*iterator2).first, temp1[i]))
+                temp2.push_back(temp1[i]);
+        }
+        for(iterator1 = (*iterator2).second.begin(); iterator1 != (*iterator2).second.end(); iterator1++;){
+            bool b = false;
+
+        }
+    }
 }
--- Text/conte_paraula.cc
+++ Text/conte_paraula.cc
@@ -6,7 +6,7 @@ bool Text::conte_paraula(string text, string paraula){
     string word;
     while (ss >> word){
         char last = word[word.size() - 1];
-        if (! ( (last >= 'a' and last <= 'z') or (last >= 'A' and last <= 'Z') )) //Checks if last char is not a letter
+        if (! ( (last >= 'a' and last <= 'z') or (last >= 'A' and last <= 'Z') or (last >= '0' and last <= '9') )) //Checks if last char is not a letter or a number
             word.erase(word.size() - 1, 1);
         if (paraula == word)
             return true;
