be1adce58d053efdcd6614f457859b106ca90bf3
Jordi Armengol Estapé <me@jordiae.com>
2016-05-25 16:49:14 +0200

    v1 de mètodes varis

--- Cjt_autors/eliminar_text.cc
+++ Cjt_autors/eliminar_text.cc
@@ -1,5 +1,5 @@
 #include "Cjt_autors.hh"
 
 void Cjt_autors::eliminar_text() {
-
+    (*it2).second.erase(it1);
 }
--- Cjt_autors/imprimeix_tots_textos.cc
+++ Cjt_autors/imprimeix_tots_textos.cc
@@ -1,5 +1,7 @@
 #include "Cjt_autors.hh"
 
 void Cjt_autors::imprimeix_tots_textos() {
-
+    for (map<string, map<string, Text> >::iterator i = Mautors.begin(); i != Mautors.end(); i++)
+    	for (map<string, Text>::iterator j = (*i).second.begin(); j != (*i).second.begin().end(); j++)
+    		cout << (*j).first << endl;
 }
