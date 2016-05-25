103b9da65d5ad6940282cabe72173691adb1a0ad
BuildTools <guillem.ferrer.nicolas@gmail.com>
2016-05-25 20:55:47 +0200

    Merge conflict fix

--- Cjt_autors/eliminar_text.cc
+++ Cjt_autors/eliminar_text.cc
@@ -2,9 +2,5 @@
 
 void Cjt_autors::eliminar_text() {
     (*it2).second.erase(it1);
-<<<<<<< HEAD
     it2 = Mautors.end();
 }
-=======
-}
->>>>>>> origin/master
--- Cjt_autors/imprimeix_nombre_frases.cc
+++ Cjt_autors/imprimeix_nombre_frases.cc
@@ -1,9 +1,5 @@
 #include "Cjt_autors.hh"
 
 void Cjt_autors::imprimeix_nombre_frases() {
-<<<<<<< HEAD
     (*it1).second.imprimeix_nombre_frases();
-=======
-	(*it2).second.(*it1).second.imprimeix_nombre_frases();
->>>>>>> origin/master
 }
