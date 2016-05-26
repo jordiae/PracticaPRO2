af3221601bf879083472f8de2641a5c714c27e99
Jordi Armengol Estapé <me@jordiae.com>
2016-05-26 22:11:34 +0200

    Cites::eliminar_cita errors de sintaxi corregits

--- Cites/eliminar_cita.cc
+++ Cites/eliminar_cita.cc
@@ -1,18 +1,15 @@
 #include "Cites.hh"
 
 void Cites::eliminar_cita(string referencia) {
-    /*
-	string referencia_sense_numero;
+	string referencia_sense_numero = referencia;
 	int n = referencia_sense_numero.size();
 	while (referencia_sense_numero[n-1] >= '0' and referencia_sense_numero[n-1] <= '9') {
 		referencia_sense_numero.pop_back();
 		n--;
 	}
 	cites.erase(referencia);
-	map<string, int>::iterator i = cites.();
 	if (referencies[referencia_sense_numero] > 0)
 		referencies[referencia_sense_numero]--;
 	else
 		referencies.erase(referencia_sense_numero);
-    */
 }
