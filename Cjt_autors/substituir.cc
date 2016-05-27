09552abc663c3d58f73cd90fa6e3b30098c06473
FerrerNicolas <guillem.ferrer.nicolas@gmail.com>
2016-05-27 11:49:29 +0200

    hello bby
    input fix

--- Cjt_autors/substituir.cc
+++ Cjt_autors/substituir.cc
@@ -2,6 +2,8 @@
 
 void Cjt_autors::substituir() {
 	string paraula_a_substituir, paraula_que_substitueix;
-	cin >> paraula_a_substituir >> paraula_que_substitueix;
+	char useless;
+	string annoying;
+	primera_linea >> useless >> paraula_a_substituir >> useless >> annoying >> useless >> paraula_que_substitueix;
 	(*it1).second.substituir(paraula_a_substituir,paraula_que_substitueix);
 }
