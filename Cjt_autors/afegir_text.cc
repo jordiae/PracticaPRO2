9a8060976c6c9bbe85cf30a4a3a109276fa7f2f2
FerrerNicolas <guillem.ferrer.nicolas@gmail.com>
2016-05-27 12:39:30 +0200

    input fix v0.8.1
    fixed text input with new method

--- Cjt_autors/afegir_text.cc
+++ Cjt_autors/afegir_text.cc
@@ -2,12 +2,10 @@
 
 void Cjt_autors::afegir_text(){
 	string nom_autor, titol_text, s;
-	char temp;
-	primera_linea >> temp;
+	primera_linea.ignore(10, '"');
 	getline(primera_linea, titol_text);
 	titol_text.erase(titol_text.size() - 1, 1);
-	cin >> s;
-	cin >> temp;
+	cin.ignore(10, '"');
 	getline(cin, nom_autor);
 	nom_autor.erase(nom_autor.size() - 1, 1);
 	Mautors[nom_autor][titol_text].afegeix_contingut();
