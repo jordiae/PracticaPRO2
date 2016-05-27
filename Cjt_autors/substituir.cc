27b6f9a5b99656bda75b06a9a49b96b0a1cc3e51
Jordi Armengol Estape <jordi.armengol.estape@a5s104mac45.fib.upc.es>
2016-05-27 11:47:28 +0200

    substituir

--- Cjt_autors/substituir.cc
+++ Cjt_autors/substituir.cc
@@ -1,7 +1,34 @@
-#include "Cjt_autors.hh"
+#include "Text.hh"
 
-void Cjt_autors::substituir() {
-	string paraula_a_substituir, paraula_que_substitueix;
-	cin >> paraula_a_substituir >> paraula_que_substitueix;
-	(*it1).second.substituir(paraula_a_substituir,paraula_que_substitueix);
+void Text::substituir(string paraula_a_substituir, string paraula_que_substitueix) {
+    int n = linies.size();
+	for (int i = 0; i < n; i++)
+		linies[i].replace(linies[i].find(paraula_a_substituir), paraula_a_substituir.size(), paraula_que_substitueix);
+	int n = frequencies.size();
+    int i = 0; 
+    bool found = false;
+    while (not found and i < n) {
+        if (frequencies[i].paraula == paraula_que_substitueix
+    	    found = false;
+        else 
+    	    i++;
+    }
+    if (found)
+        frequencies[i].freq++;
+    else {
+        frequencia f;
+        f.paraula = paraula_que_substitueix;
+        f.freq = 1;
+        frequencies.push_back(f);
+    }
+    for (int i = 0; i < n; i++) {
+    	if (frequencies[i].paraula == paraula_a_substituir and frequencies[i].freq > 1)
+    		frequencies[i].freq--;
+    	if (frequencies[i].paraula == paraula_a_substituir and frequencies[i].freq <= 1) {
+    		for (int j = i; j < n-1; j++)
+    			frequencies[j] == frequencies[j+1];
+    		frequencies.pop_back();
+    	}
+
+    }
 }
