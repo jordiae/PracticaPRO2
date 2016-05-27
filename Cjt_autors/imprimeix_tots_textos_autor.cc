164c87724e4736f39411c58d03b6e767be75e84e
FerrerNicolas <guillem.ferrer.nicolas@gmail.com>
2016-05-27 12:09:10 +0200

    input fix v 0.8
    two classes down, I guess.

--- Cjt_autors/imprimeix_tots_textos_autor.cc
+++ Cjt_autors/imprimeix_tots_textos_autor.cc
@@ -2,7 +2,9 @@
 
 void Cjt_autors::imprimeix_tots_textos_autor() {
 	string autor;
-	cin >> autor;
+	primera_linea.ignore(10, '"');
+	primera_linea >> autor;
+	autor.erase(autor.size()-1, 1);
 	map<string, Text>::iterator it;
 	for (it = it2 -> second.begin(); it != it2 -> second.end(); it++)
 		cout << it -> first << endl;
--- Cjt_autors/substituir.cc
+++ Cjt_autors/substituir.cc
@@ -2,8 +2,11 @@
 
 void Cjt_autors::substituir() {
 	string paraula_a_substituir, paraula_que_substitueix;
-	char useless;
-	string annoying;
-	primera_linea >> useless >> paraula_a_substituir >> useless >> annoying >> useless >> paraula_que_substitueix;
+	primera_linea.ignore(10, '"');
+	primera_linea >> paraula_a_substituir;
+	paraula_a_substituir.erase(paraula_a_substituir.size() - 1, 1);
+	primera_linea.ignore(10, '"');
+	primera_linea >> paraula_que_substitueix;
+	paraula_que_substitueix.erase(paraula_que_substitueix.size() - 1, 1);
 	(*it1).second.substituir(paraula_a_substituir,paraula_que_substitueix);
 }
