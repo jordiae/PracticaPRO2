17dbc03b854a09669d41c2f364e4792b2576eecc
BuildTools <guillem.ferrer.nicolas@gmail.com>
2016-05-28 18:37:12 +0200

    Output fix
    The texts were missing the author name and the quotiation marks

--- Cjt_autors/imprimeix_tots_textos.cc
+++ Cjt_autors/imprimeix_tots_textos.cc
@@ -2,6 +2,7 @@
 
 void Cjt_autors::imprimeix_tots_textos() {
     for (map<string, map<string, Text> >::iterator i = Mautors.begin(); i != Mautors.end(); i++)
-    	for (map<string, Text>::iterator j = (*i).second.begin(); j != (*i).second.end(); j++)
-    		cout << (*j).first << endl;
+    	for (map<string, Text>::iterator j = (*i).second.begin(); j != (*i).second.end(); j++){
+            cout << (*i).first << " \"" << (*j).first << "\"" << endl;
+    	}
 }
