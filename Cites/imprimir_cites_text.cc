00e83a3724ab190193a14f1f56bc6b63606df896
BuildTools <guillem.ferrer.nicolas@gmail.com>
2016-05-28 21:53:06 +0200

    output fix

--- Cites/imprimir_cites_text.cc
+++ Cites/imprimir_cites_text.cc
@@ -3,11 +3,10 @@
 void Cites::imprimir_cites_text(string titol_text) {
 	for (map<string, cita>::const_iterator i = cites.begin(); i != cites.end(); i++) {
 		if ((*i).second.titol_text == titol_text) {
-			cout << (*i).second.autor << endl << (*i).second.titol_text << endl << (*i).second.frase_inicial << endl << (*i).second.frase_final << endl;
+            cout << (*i).first << endl;
 			int n = (*i).second.frases.size();
 			for (int k = 0; k < n; k++)
-	    		cout << (*i).second.frases[k];
-			cout << endl;
+	    		cout << (*i).second.frase_inicial +k << " " << (*i).second.frases[k] << endl;;
 		}
 	}
 }
